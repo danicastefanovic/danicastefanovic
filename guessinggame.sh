075276e346e889b25ac1ea10c142235d7d9f2204
danicastefanovic <64682212+danicastefanovic@users.noreply.github.com>
2020-06-19 13:52:14 +0200

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,21 +2,23 @@ clear
 declare -i numFiles
 numFiles=$(find . -maxdepth 1 -not -type d | wc --lines)
 
-function makeVaildGuess {
-   until read -p "Uneti koliko datoteka ima u trenutnom direktorijumu: " guess;
-   [[ "$guess" =~ ^ [0-9] + $ ]] ; do
-        echo -e "\nUnesite nenegativnu celobrojnu vrednost!\n"
-   done
+function
+guess {
+echo "Can you guess the number of files in current directory? Your
+gues is: "
+n1=$(ls -F |grep -v / | wc -l)
+while read n2; do
+if [[ $n2 -eq $n1 ]]; then
+echo "Congratulations"
+break;
+else
+echo
+if [[ $n2 -gt $n1 ]]; then
+echo -n "Your guess is too high. New guess:"
+elif [[ $n2 -lt $n1 ]]; then
+echo -n "Your guess is too low. New guess:"
+fi
+fi
+done
 }
-while makeVaildGuess; do
-    if (( guess < numFiles )) ; then 
-       echo -e "\nVasa procena je premala."
-    elif (( guess > numFiles )) ; then
-       echo -e "\nVasa procena je prevelika."
-    else 
-       echo -e "\nCestitke! Odgovor $numFiles je tacan!"
-       exit
-    fi
- done
-   
-
+Guess
